511b79ebe990ae797fdcb43a9843c0e18020721b
Darío Rivera <fermius.us@gmail.com>
2022-04-07 08:30:16 -0500

    feat: create dart package scaffolding

---
+++ example/layer_contracts_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:layer_contracts_dart/layer_contracts_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/layer_contracts_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library layer_contracts_dart;
+
+export 'src/layer_contracts_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
